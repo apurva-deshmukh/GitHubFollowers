dc25b73b4879c602a039d35abf2e8e5c7886f0de
apurva-deshmukh <aadeshmukh13@gmail.com>
2022-03-27 21:11:02 -0400

    initial setup

---
+++ GHFollowers/FavoriteListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteListViewController.swift
+//  GHFollowers
+//
+//  Created by Apurva Deshmukh on 3/27/22.
+//
+
+import UIKit
+
+class FavoriteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  GHFollowers
+//
+//  Created by Apurva Deshmukh on 3/27/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
