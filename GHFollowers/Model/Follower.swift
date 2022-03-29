b71d4eaed3097148132b5be295d9b8f7736dfe3d
apurva-deshmukh <aadeshmukh13@gmail.com>
2022-03-28 21:21:48 -0400

    create user and followers objects

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Apurva Deshmukh on 3/28/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
